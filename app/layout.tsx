60a91f366a6f959f66609b8eea7bf31434e79ddf
edpau <edpau@me.com>
2023-10-07 21:11:46 +0100

    revise on the layout to orignial

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from '../lib/components/navbar'
-import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,9 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
     
-      <Head>
-      <link rel="icon" href="/favicon.ico" />
-      </Head>
+  
 
     
       <body className={inter.className}>
