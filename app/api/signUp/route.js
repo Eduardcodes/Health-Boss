b1143ff09583a00cc9fb108516a65871c690eb3f
edpau <edpau@me.com>
2023-10-07 12:14:06 +0100

    feat: signUp can post to db

--- app/api/signUp/route.js
+++ app/api/signUp/route.js
@@ -1,14 +1,15 @@
 //url: http://localhost:3000/api/signUp
 
-import prisma from "@/lib/components/prismadb"
+import prisma from "@/lib/components/prismadb";
 import { NextResponse } from "next/server"
 
 export const POST = async (request) => {
     try {
         const body = await request.json();
         const {userName, email} = body;
-
-        const newUser = await prisma.post.create({
+        
+        console.log("hi")
+        const newUser = await prisma.user.create({
             data: {
                 userName,
                 email
@@ -18,6 +19,7 @@ export const POST = async (request) => {
         return NextResponse.json(newUser);
 
     } catch (error) {
+        console.log(error)
         return NextResponse.json({message: "POST Error", error}, {status:500})
         
     }
