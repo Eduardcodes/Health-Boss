f876ca68f0b99f2336696d176b85f3b6a2d7ef81
edpau <edpau@me.com>
2023-10-07 22:33:21 +0100

    feat: Model compenent draft

--- app/ed/components/AddUser.jsx
+++ app/ed/components/AddUser.jsx
@@ -1,18 +1,86 @@
-"use client"
+"use client";
 
 import { useState } from "react";
-
-import { useRouter} from 'next/navigation';
-
+import axios from "axios";
+import Modal from "./Modal"
+import { useRouter } from "next/navigation";
 
 const AddUser = () => {
+  const router = useRouter();
 
-    return (
-        <div>
-            <h1>12</h1>
-        </div>
-    )
+  const [modalOpen, setModalOpen] = useState(false);
+  const [inputs, setInputs] = useState({});
 
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("api/post", inputs)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setInputs({});
+        setModalOpen(false);
+        router.refresh();
+      });
+  };
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((prevState) => ({...prevState, [name]: value}))
+  }
+
+  return (
+    <div>
+      <button
+         onClick = {() => setModalOpen(true)}
+         className="bg-blue-700 text-white p-3 cursor-pointer"
+      >
+     Add New User
+      </button>
+
+      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+        <form className="w-full" onSubmit={handleSubmit}>
+        <h1 className="text-2xl pb-3">Add New User</h1>
+
+        <input 
+            type="text"
+            placeholder="Username"
+            name="userName"
+            className="w-full p-2"
+            value = {inputs.userName || ""}
+            onChange={handleChange}
+        />
+
+        <input 
+            type="text"
+            placeholder="Email"
+            name="email"
+            className="w-full p-2"
+            value = {inputs.email || ""}
+            onChange={handleChange}
+        />
+
+        <input 
+            type="text"
+            placeholder="Password"
+            name="password"
+            className="w-full p-2"
+            value = {inputs.password || ""}
+            onChange={handleChange}
+        />
+        
+        <button type="submit" className="bg-blue-700 text-white px-5 py-2">
+            Submit
+        </button>
+        </form>
+      </Modal>
+    </div>
+  );
+};
 
 export default AddUser;
