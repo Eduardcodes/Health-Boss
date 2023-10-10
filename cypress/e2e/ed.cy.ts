ea0268a4cde1c1f0db86c7143b38983110a4bb64
edpau <edpau@me.com>
2023-10-10 08:39:08 +0100

    update cypress test on create user, seperate test into three parts

--- cypress/e2e/ed.cy.ts
+++ cypress/e2e/ed.cy.ts
@@ -1,27 +1,37 @@
-describe("user not log in in homepage", () => {
-    it("Visits all the nav bar", () => {
+describe("Add, edit and delete user", () => {
+  it("test1", () => {
     cy.visit("http://localhost:3000/ed");
     cy.contains("Add New User").click();
-    cy.get('input[name="userName"]').type('testName')
-    cy.get('input[name="userName"]').should('have.value', 'testName')
-    cy.get('input[name="email"]').type('testEmail')
-    cy.get('input[name="email"]').should('have.value', 'testEmail')
-    cy.get('input[name="password"]').type('testpassword')
-    cy.get('input[name="password"]').should('have.value', 'testpassword')
+    cy.get('input[name="userName"]').type("testName");
+    cy.get('input[name="userName"]').should("have.value", "testName");
+    cy.get('input[name="email"]').type("testEmail");
+    cy.get('input[name="email"]').should("have.value", "testEmail");
+    cy.get('input[name="password"]').type("testpassword");
+    cy.get('input[name="password"]').should("have.value", "testpassword");
     cy.contains("Submit").click();
     cy.wait(1000);
-    cy.get('button.text-blue-700').last().click();
-    cy.get('input[name="userName"]').should('have.value', 'testName')
-    cy.get('input[name="email"]').should('have.value', 'testEmail')
-    cy.get('input[name="userName"]').type('1')
-    cy.get('input[name="userName"]').should('have.value', 'testName1')
-    cy.get('input[name="email"]').type('1')
-    cy.get('input[name="email"]').should('have.value', 'testEmail1')
+  });
+
+  it("edit new user", () => {
+    cy.visit("http://localhost:3000/ed");
+    cy.wait(1000);
+    cy.get("button.text-blue-700").last().click();
+    cy.get('input[name="userName"]').should("have.value", "testName");
+    cy.get('input[name="email"]').should("have.value", "testEmail");
+    cy.get('input[name="userName"]').type("1");
+    cy.get('input[name="userName"]').should("have.value", "testName1");
+    cy.get('input[name="email"]').type("1");
+    cy.get('input[name="email"]').should("have.value", "testEmail1");
     cy.contains("Submit").click();
+    cy.wait(1000);
+  });
+
+  it("delete new user", () => {
+    cy.visit("http://localhost:3000/ed");
     cy.wait(1000);
     cy.get("button.text-red-700").last().click();
     cy.contains("Yes").click();
     cy.wait(1000);
-    cy.get('body').should('not.contain', 'testName');
-    });
-})
+    cy.get("h1").should("not.contain", "testName");
+});
+});
